bd15050433a23649210cdc8033684a5b1a08687b
Sergey <sergey.melnikov@krit.pro>
2019-03-28 12:22:46 +0400

    user's mail added to columns option interface

--- init.rb
+++ init.rb
@@ -1,5 +1,6 @@
 require 'redmine'
 require_relative 'lib/queries_helper_patch'
+require_relative 'lib/time_entry_query_patch'
 
 # ActionDispatch::Callbacks.to_prepare do                for Rails 5.0 -- deprecoted TODO sim need testing
 # ActiveSupport::Reloader.to_prepare do                  for Rails 5.1
@@ -8,11 +9,15 @@ reloader.to_prepare do
   QueriesHelper.send :include, QueriesHelperPatch
 end
 
+reloader.to_prepare do
+  TimeEntryQuery.send :include, TimeEntryQueryPatch
+end
+
 Redmine::Plugin.register :full_user_inf_to_csv do
   name 'Full User information upload to .csv plugin'
   author 'Sergey Melnikov'
   description 'This is a plugin for Redmine. Added users custom fields to time_entry report'
-  version '0.0.1'
+  version '0.0.2'
   url 'https://github.com/SimSmolin/full_user_inf_to_csv.git'
   author_url 'https://github.com/SimSmolin'
 end
--- lib/queries_helper_patch.rb
+++ lib/queries_helper_patch.rb
@@ -19,11 +19,11 @@ module QueriesHelperPatch
         if items.first.is_a?(TimeEntry)
           # csv header fields
           temp_csv = columns.map {|c| c.caption.to_s}
-          csv << temp_csv + User.current.visible_custom_field_values.collect { |c| l(:field_user)+':'+c.custom_field.name } + [l(:field_user)+':'+l(:field_mail)]
+          csv << temp_csv + User.current.visible_custom_field_values.collect { |c| l(:field_user)+':'+c.custom_field.name }
           # csv lines
           items.each do |item|
             temp_csv = columns.map {|c| csv_content(c, item)}
-            csv << temp_csv + item.user.custom_field_values.collect { |v| v.value } + [item.user.mail]
+            csv << temp_csv + item.user.custom_field_values.collect { |v| v.value }
           end
         else
           # csv header fields
